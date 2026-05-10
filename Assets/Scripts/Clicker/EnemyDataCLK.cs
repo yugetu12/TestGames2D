430c3e5c34946a9907e366c69a116e3947b88192
Yuzuki Kaya <b1025054@fun.ac.jp>
2026-05-10 16:37:23 +0900

    EnemyManagerCLK.cs ->講座用に単体で動くように調整 ->書きやすいように修正

--- Assets/Scripts/Clicker/EnemyDataCLK.cs
+++ Assets/Scripts/Clicker/EnemyDataCLK.cs
@@ -1,10 +1,9 @@
-using UnityEngine;
+// using UnityEngine;
 
-[CreateAssetMenu(fileName = "EnemyDataCLK", menuName = "Scriptable Objects/EnemyDataCLK")]
-public class EnemyDataCLK : ScriptableObject
-{
-    public string enemyName;    //敵の名前
-    public int maxHealth;       //敵の最大HP
-    public Sprite enemySprite;  //敵の画像
-    public int exPoints;        //敵の経験値量
-}
+// [CreateAssetMenu(fileName = "EnemyDataCLK", menuName = "Scriptable Objects/EnemyDataCLK")]
+// public class EnemyDataCLK : ScriptableObject
+// {
+//     public string enemyName;    //敵の名前
+//     public int maxHealth;       //敵の最大HP
+//     public Sprite enemySprite;  //敵の画像
+// }
--- Assets/Scripts/Clicker/EnemyManagerCLK.cs
+++ Assets/Scripts/Clicker/EnemyManagerCLK.cs
@@ -3,44 +3,41 @@ using UnityEngine.UI;
 
 public class EnemyManagerCLK : MonoBehaviour
 {
-    [SerializeField] private int maxHealth = 100;   //敵の最大HP
-    private int health;                             //敵の現在のHP
-    public EnemyDataCLK enemyData;                  //敵のマスターデータ
-    [SerializeField] private Text enemyNameText;
-    [SerializeField] private Text healthText;
-    [SerializeField] private Image enemyImage;
+    public string enemyName;                    //敵の名前
+    public int maxHealth;                       //敵の最大HP
+    public Sprite enemySprite;                  //敵の画像
+    [SerializeField] private Text nameText;     //敵の名前を表示するテキストコンポーネント
+    [SerializeField] private Text healthText;   //敵のHPを表示するテキストコンポーネント
+    [SerializeField] private Image enemyImage;  //敵の画像を表示するイメージコンポーネント
+    private int health;                         //敵のHP
 
     void Start()
     {
-        //HPを初期化する
+        //HPを初期化する (1)
         health = maxHealth;
+
         //敵の名前を設定する
-        enemyNameText.text = enemyData.enemyName;
-        //HPテキストを更新する
-        TakeDamage(0);
+        nameText.text = enemyName;
         //敵の画像を設定する
-        enemyImage.sprite = enemyData.enemySprite;
+        enemyImage.sprite = enemySprite;
+        //HPテキストを更新する (2)
+        TakeDamage(0);
     }
 
     public void TakeDamage(int damage)
     {
-        //ダメージを適用する
+        //ダメージを適用する (3)
         health -= damage;
 
-        //HPが0以下になったら死ぬ
+        //HPが0以下になったら死ぬ (4)
         if (health <= 0)
         {
-            Die();
+            health = 0;
+            Debug.Log(enemyName + "を倒した！");
         }
 
         if (healthText == null) return;
         //HPテキストを更新する
         healthText.text = health.ToString() + " / " + maxHealth.ToString();
-    }
-
-    private void Die()
-    {
-        //敵が死んだときの処理
-        Debug.Log("敵が倒れた！");
     }
 }
